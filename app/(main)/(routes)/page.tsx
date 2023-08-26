923ff42c0c67bcc18e8282c0beaf7a5c99344333
RajrupDasid <rajrupofficialid@protonmail.com>
2023-08-26 21:30:47 +0530

    Ui modification
    Dark mode and some other ui modification added
    
    DC-1
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
